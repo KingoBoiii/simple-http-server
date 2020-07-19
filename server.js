4004cc5e389dae8c7b44b5172cc531e19faef247
Victor Krogh Hansen <Victor Hansen@kingo.dev>
2020-07-19 20:30:47 +0200

    Added some BASIC routing

--- server.js
+++ server.js
@@ -2,13 +2,22 @@ const http = require('http');
 
 const server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!');
-    console.log('Request made!');
+    
+    console.log(`Request made! Url: ${req.url}`);
+    switch(req.url) {
+        case '/': res.end('Index Page'); break;
+        case '/about': res.end('About Page'); break;
+        case '/contact': res.end('Contact Page'); break;
+        default: res.end('404 - Page not found'); break;
+    }
 });
 
-server.listen(3000, function(err) { 
+server.listen(3000, 'localhost', function(err) { 
     if(err) {
         return console.log(`Error: ${err}`);
     }
-    console.log('Server started, and listening for requests on port: 3000'); 
+
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log('Server started and listening for requests on http://%s:%s', host, port); 
 });
